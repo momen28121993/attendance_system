3ab637b1bbabc99115e629bf903564106243ec50
MoamenAhmed <youssef2812ahmed@gmail.com>
2025-12-10 17:28:01 +0200

    Disable anti-spoofing by default to unblock recognition

--- face_attendance_main.py
+++ face_attendance_main.py
@@ -48,7 +48,7 @@ class Config:
     FRAME_HEIGHT = 480
 
     # Anti-spoofing (SilentFace)
-    ANTI_SPOOF_ENABLED = True
+    ANTI_SPOOF_ENABLED = False
     ANTI_SPOOF_THRESHOLD = 0.5
     ANTI_SPOOF_MODEL_URL = (
         "https://raw.githubusercontent.com/minivision-ai/Silent-Face-Anti-Spoofing/master/resources/anti_spoof_models/"
